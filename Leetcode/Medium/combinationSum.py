91dbbcc65216e1e3bab0c0a4bbced4cee61a60b5
Shriya Sehgal <33370939+Shriyasehgal@users.noreply.github.com>
2022-01-25 21:24:24 -0500

    Update combinationSum.py
    Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
    
    The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
    
    It is guaranteed that the number of unique combinations that sum up to target is less than 150 combinations for the given input.

--- Leetcode/Medium/combinationSum.py
+++ Leetcode/Medium/combinationSum.py
@@ -24,3 +24,27 @@ class Solution:
         helper(0)
         return res
             
+   #Optimized solution.
+  class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        subset = []
+        n = len(candidates)
+        listsum = 0
+        def helper(i):
+            nonlocal listsum
+            if listsum > target:
+                return 
+            if listsum == target:
+                res.append(subset[:])
+            for j in range(i,n):
+                listsum += candidates[j]
+                subset.append(candidates[j])
+                helper(j)
+                subset.pop()
+                listsum-=candidates[j]
+                
+            
+        helper(0)
+        return res
+            
