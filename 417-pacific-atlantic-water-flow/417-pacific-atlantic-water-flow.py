da5c033289d670388c303448028d32120a7c82cc
Shriya Sehgal <33370939+Shriyasehgal@users.noreply.github.com>
2022-10-03 15:27:48 -0400

    Time: 566 ms (43.54%), Space: 15.8 MB (35.74%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -1,35 +1,28 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        pac = False
-        atl = False
+        rows,cols = len(heights), len(heights[0])
+        pac,atl = set(),set()
+        
         def dfs(i,j,visited):
-            nonlocal pac,atl
-            
             if (i,j) in visited:
-                return 
+                return
             visited.add((i,j))
-            if i == 0 or j == 0:
-                pac = True
-            if i == len(heights)-1 or j == len(heights[0])-1:
-                atl = True
-            if i > 0 and heights[i][j] >= heights[i-1][j]:
+            if i > 0 and heights[i][j] <= heights[i-1][j]:
                 dfs(i-1,j,visited)
-            if i < len(heights)-1 and heights[i][j] >= heights[i+1][j]:
+            if i < len(heights)-1 and heights[i][j] <= heights[i+1][j]:
                 dfs(i+1,j,visited)
-            if j > 0 and heights[i][j] >= heights[i][j-1]:
+            if j > 0 and heights[i][j] <= heights[i][j-1]:
                 dfs(i,j-1,visited)
-            if j < len(heights[0]) -1 and heights[i][j] >= heights[i][j+1]:
+            if j < len(heights[0])-1 and heights[i][j] <= heights[i][j+1]:
                 dfs(i,j+1,visited)
                 
-        res = []
-
-        for i in range(len(heights)):
-            for j in range(len(heights[0])):
-                pac = False
-                atl = False
-                dfs(i,j,visited=set())
-                if pac == True and atl == True:
-                    res.append([i,j])
-        return res
+        for i in range(rows):
+            dfs(i,0,pac)
+            dfs(i,cols-1,atl)
+        for j in range(cols):
+            dfs(0,j,pac)
+            dfs(rows-1,j,atl)
+        return list(pac&atl)
+            
                 
             
