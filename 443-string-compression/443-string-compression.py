ee7e88b763dcbddf360a6aed59d600e72aea7109
Shriya Sehgal <33370939+Shriyasehgal@users.noreply.github.com>
2022-10-08 16:48:18 -0400

    Time: 57 ms (97.71%), Space: 13.9 MB (97.27%) - LeetHub

--- 443-string-compression/443-string-compression.py
+++ 443-string-compression/443-string-compression.py
@@ -1,28 +1,23 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        count = 1
-        cur = chars[0]
+        idx = 0
         i = 0
-        j = 1
         while i < len(chars):
-            if j < len(chars) and cur == chars[j]:
-                chars.pop(j)
-                count+=1
-            else:
-                idx = j
-                if count > 1:
-                    while count != 0:
-                        temp = count % 10
-                        count //=10
-                        chars.insert(idx,str(temp))
-                        j+=1
-                i = j
-                if j < len(chars):
-                    cur = chars[j]
-                    j+=1
-                    count = 1
-        return len(chars)
-        
+            j = i
+            while j < len(chars) and chars[j] == chars[i]:
+                j+=1
+            chars[idx] = chars[i]  
+            idx+=1
+            if j-i > 1:
+                count = str(j-i)
+                for c in count:
+                    chars[idx] = c
+                    idx+=1
+            i = j
+        return idx
+            
+                    
+                
         
                     
                     
