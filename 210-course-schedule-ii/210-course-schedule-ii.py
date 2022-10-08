ddde12fb45a215019a96de96fb1584f2bf103206
Shriya Sehgal <33370939+Shriyasehgal@users.noreply.github.com>
2022-10-08 15:59:08 -0400

    Time: 112 ms (87.16%), Space: 17.4 MB (32.00%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,38 +1,28 @@
 class Solution:
     def buildGraph(self, numCourses, prerequisites):
-        courseGraph = {k:[] for k in range(numCourses) }
-        for pre in prerequisites:
-            c1,c2 = pre
-            courseGraph[c1].append(c2)
-        return courseGraph
+        graph = defaultdict(list)
+        for a,b in prerequisites:
+            graph[a].append(b)
+        return graph
     
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        graph = self.buildGraph(numCourses, prerequisites) # to build an ajc list
-        visited = [0]*numCourses # This is to check for any cycle
-        checked = [0]*numCourses # This is for dfs to add the courses in the output.
-        output = [] # This is to add the course in the order
-        def buildOrder(node):
-            #There is a cycle
-            if visited[node] == 1:
-                return False 
-            #We have added the course in the output. So we dont add the courses twice.
-            if checked[node] == 1:
-                return True
-            visited[node] = 1
-            #For all the prereq of the courses.
-            for n in graph[node]:
-                if not buildOrder(n):
+        graph = self.buildGraph(numCourses, prerequisites)
+        visited = set() #to check the current cycle
+        taken = set() # to check which courses we have already taken
+        res = []
+        def helper(i):
+            if i in taken: return True
+            if i in visited : return False
+            visited.add(i)
+            for n in graph[i]:
+                if not helper(n):
                     return False
-            #We are no longer checking this path, so change it back to 0
-            visited[node] = 0
-            #All the prereq have been added to the list, so now we can add this course.
-            checked[node] = 1
-            output.append(node)
+            taken.add(i)
+            res.append(i)
+            graph[i] = []
+            visited.remove(i)
             return True
-        
-        for c in range(numCourses):
-            if buildOrder(c) == False:
-                return []
-        return output
-        
-            
+        for i in range(numCourses):
+            if not helper(i): return []
+        return res
+                
