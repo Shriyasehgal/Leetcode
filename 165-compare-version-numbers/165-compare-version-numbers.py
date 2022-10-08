cb2bc1ba78bae48a7cb602106723dbb598bb6bc1
Shriya Sehgal <33370939+Shriyasehgal@users.noreply.github.com>
2022-10-08 10:03:03 -0400

    Time: 40 ms (77.16%), Space: 13.9 MB (77.80%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.py
+++ 165-compare-version-numbers/165-compare-version-numbers.py
@@ -3,9 +3,9 @@ class Solution:
         v1 = version1.split('.')
         v2 = version2.split('.')
         
-        for i in range(max(len(v1),len(v2))):
-            a = int(v1[i]) if i < len(v1) else 0
-            b = int(v2[i]) if i < len(v2) else 0
+        for a,b in zip_longest(v1,v2,fillvalue='0'):
+            a = int(a)
+            b = int(b)
             if a == b: continue
             elif a > b: return 1
             else: return -1
