f17b83c594b642e34e4637c1408588bf2036abb3
Shriya Sehgal <33370939+Shriyasehgal@users.noreply.github.com>
2022-10-24 20:26:36 -0400

    Time: 87 ms (5.01%), Space: 13.7 MB (98.50%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
@@ -1,12 +1,17 @@
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
-        queue = deque([])
-        for word in word1:
-            for char in word:
-                queue.append(char)
-        for word in word2:
-            for char in word:
-                if not queue: return False
-                if char != queue.popleft(): return False
-        return not queue
+        i1 = j1 = i2 = j2 = 0
+        while i1 < len(word1) and i2< len(word2):
+            if word1[i1][j1] != word2[i2][j2]: return False
+            j1+=1
+            j2+=1
+            if j1>=len(word1[i1]): 
+                j1 = 0
+                i1 += 1
+            if j2>= len(word2[i2]):
+                j2 = 0
+                i2 += 1
+            
+            if i1 == len(word1) and i2 == len(word2): return True
+        return False
                 
