9c303494d1f44f37a2798007d64e3772df0e4d2c
Shriya Sehgal <33370939+Shriyasehgal@users.noreply.github.com>
2022-11-16 13:26:35 -0500

    Time: 131 ms (30.23%), Space: 14.4 MB (92.77%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,7 +1,25 @@
 class Solution:
     
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        graph = defaultdict(list)
+        parent = [i for i in range(len(edges)+1)]
+        def find(x):
+            p = parent[x]
+            if x == p:
+                return p
+            else: return find(p)
+        def union(x,y):
+            p1 = find(x)
+            p2 = find(y)
+            if p1 == p2:
+                return True
+            else:
+                parent[p2] = p1
+            return False
+            
+        for x,y in edges:
+            if union(x,y): return [x,y]
+            
+        ''' graph = defaultdict(list)
         for a,b in edges:
             if not self.dfs(a,b,graph, set()):
                 return [a,b]
@@ -17,4 +35,4 @@ class Solution:
         visited.add(start)
         for n in graph[start]:
             if not self.dfs(n,target,graph,visited): return False
-        return True
+        return True'''
